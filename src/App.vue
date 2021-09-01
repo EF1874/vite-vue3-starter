db4821753b96f344bdca4f766d6feff8eb2e19f9
licong <284264018@qq.com>
2021-09-01 15:58:48 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -2,27 +2,32 @@
  * @Author: 李聪
  * @Date: 2021-08-31 10:57:39
  * @LastEditors: 李聪
- * @LastEditTime: 2021-09-01 11:35:34
+ * @LastEditTime: 2021-09-01 15:58:16
  * @Description:
 -->
 <script setup lang="ts"></script>
 
 <template>
+  <img class="logo" src="@a/logo.png" alt="logo" />
   <router-link to="/" class="nav">首页</router-link>
   <router-link :to="{ name: 'About' }" class="nav">About</router-link>
   <router-view></router-view>
 </template>
 
-<style>
+<style lang="less">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
-}
-.nav {
-  margin-right: 10px;
+  .logo {
+    display: block;
+    margin: auto;
+    margin-top: 60px;
+  }
+  .nav {
+    margin-right: 10px;
+  }
 }
 </style>
